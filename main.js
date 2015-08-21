34e6616715003ef1eb788d8da5a30b72f45a753d
Geoff Morris <geoffm@gmail.com>
2015-08-21 12:50:16 -0700

    Added Jquery, some style, and mis.js
    query can, full screen style, mis as a js object

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-var width = 960,
-    height = 500;
+var MY_TYPE = "A";
+var IP_GROUP = 1;
+var URL_GROUP = 2;
+
+//var width = 960,
+//    height = 500;
+
+var width = $(window).width();
+    height = $(window).height();
 
 var color = d3.scale.category20();
 
@@ -12,38 +19,109 @@ var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-d3.json("miserables.json", function(error, graph) {
-  if (error) throw error;
+var dnsNodes,
+    ipNodes,
+    urlNodes,
+    dnsLinks;
 
-  force
-      .nodes(graph.nodes)
-      .links(graph.links)
-      .start();
+var parsedData = {"nodes":[],"links":[]};
 
-  var link = svg.selectAll(".link")
-      .data(graph.links)
-    .enter().append("line")
-      .attr("class", "link")
-      .style("stroke-width", function(d) { return Math.sqrt(d.value); });
 
-  var node = svg.selectAll(".node")
-      .data(graph.nodes)
-    .enter().append("circle")
-      .attr("class", "node")
-      .attr("r", 5)
-      .style("fill", function(d) { return color(d.group); })
-      .call(force.drag);
 
-  node.append("title")
-      .text(function(d) { return d.name; });
 
-  force.on("tick", function() {
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+//create nodes and links:
+//From...
 
-    node.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-  });
+// {
+//     "name": "verizonwireless.com.",
+//     "value": "137.188.80.90",
+//     "TTL": "300",
+//     "type": "A"
+//   },
+
+//To...
+
+//{"nodes":[{"name":"128.12312.12.31.23","group":IP_GROUP},
+//          {"name":"somewebsite.verizonwireless.com.","group":URL_GROUP}]
+//"links":[{"source":1,"target":0,"value":1},...]}
+
+$.each(dns_records, function( index, dnsEntry ) {
+  if(dnsEntry.type==MY_TYPE) {
+    //create a new ipNode
+    // dnsEntry.
+  }
 });
+
+
+ 
+graph = mis; //loaded js with script tag in index.html
+
+//d3 stuffs
+force
+    .nodes(graph.nodes)
+    .links(graph.links)
+    .start();
+
+var link = svg.selectAll(".link")
+    .data(graph.links)
+  .enter().append("line")
+    .attr("class", "link")
+    .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+var node = svg.selectAll(".node")
+    .data(graph.nodes)
+  .enter().append("circle")
+    .attr("class", "node")
+    .attr("r", 5)
+    .style("fill", function(d) { return color(d.group); })
+    .call(force.drag);
+
+node.append("title")
+    .text(function(d) { return d.name; });
+
+force.on("tick", function() {
+  link.attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+
+  node.attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+});
+
+
+// d3.json("miserables.json", function(error, graph) {
+//   if (error) throw error;
+
+//   force
+//       .nodes(graph.nodes)
+//       .links(graph.links)
+//       .start();
+
+//   var link = svg.selectAll(".link")
+//       .data(graph.links)
+//     .enter().append("line")
+//       .attr("class", "link")
+//       .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+//   var node = svg.selectAll(".node")
+//       .data(graph.nodes)
+//     .enter().append("circle")
+//       .attr("class", "node")
+//       .attr("r", 5)
+//       .style("fill", function(d) { return color(d.group); })
+//       .call(force.drag);
+
+//   node.append("title")
+//       .text(function(d) { return d.name; });
+
+//   force.on("tick", function() {
+//     link.attr("x1", function(d) { return d.source.x; })
+//         .attr("y1", function(d) { return d.source.y; })
+//         .attr("x2", function(d) { return d.target.x; })
+//         .attr("y2", function(d) { return d.target.y; });
+
+//     node.attr("cx", function(d) { return d.x; })
+//         .attr("cy", function(d) { return d.y; });
+//   });
+// });
